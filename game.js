00c1df2d3f2d9397d25f89356b1544ed2b79ef32
Daniel Gollahon <Daniel.Gollahon@gmail.com>
2017-07-29 16:22:43 -0700

    map thing

--- game.js
+++ game.js
@@ -1,79 +1,127 @@
 "use strict";
-var canvas = document.getElementById("canvas");
-var context = canvas.getContext("2d");
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
 // Game constants
-var WIDTH = 720;
-var HEIGHT = 480;
-var TILE_SIZE = 32;
-// Supporting classes
-var Player = (function () {
-    function Player() {
+const WIDTH = 720;
+const HEIGHT = 480;
+const TILE_SIZE = 24;
+class Level {
+    constructor() {
+        this.data = [
+            "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+            "X                            X",
+            "X                            X",
+            "X                            X",
+            "X                            X",
+            "X                            X",
+            "X                            X",
+            "X                            X",
+            "X                            X",
+            "X                            X",
+            "X                            X",
+            "X                            X",
+            "X                            X",
+            "X                            X",
+            "X                            X",
+            "X                            X",
+            "X                           XX",
+            "X           XXXX  XXXX     XXX",
+            "X           X  X  X  X    XXXX",
+            "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+        ];
     }
-    Player.prototype.draw = function () {
+    draw(context) {
+        context.save();
+        context.fillStyle = "#0000FF";
+        context.strokeStyle = "#FFFFFF";
+        let y = 0 * TILE_SIZE;
+        this.data.forEach(rowData => {
+            for (let col = 0; col < 30; ++col) {
+                if (rowData[col] === "X") {
+                    context.fillRect(col * TILE_SIZE, y, TILE_SIZE, TILE_SIZE);
+                }
+            }
+            y += TILE_SIZE;
+        });
+        context.restore();
+    }
+}
+// Supporting classes
+class Player {
+    constructor() {
+        this.x = WIDTH / 2;
+        this.y = HEIGHT - TILE_SIZE / 2;
+        this.ySpeed = 0;
+        this.xSpeed = 3.5;
+        this.jumping = false;
+    }
+    draw() {
         context.save();
         context.fillStyle = "#000000";
         context.fillRect(this.x - TILE_SIZE / 2, this.y - TILE_SIZE / 2, TILE_SIZE, TILE_SIZE);
         context.restore();
-    };
-    return Player;
-}());
+    }
+    update(keyState) {
+        if (keyState.isDown(KeyCode.A)) {
+            this.x -= this.xSpeed;
+        }
+        else if (keyState.isDown(KeyCode.D)) {
+            this.x += this.xSpeed;
+        }
+        else if (keyState.isDown(KeyCode.W)) {
+            if (this.ySpeed === 0 && !this.jumping) {
+                this.ySpeed = -15;
+                this.jumping = true;
+            }
+        }
+        this.y += this.ySpeed;
+        if (this.y > HEIGHT - TILE_SIZE / 2) {
+            this.y = HEIGHT - TILE_SIZE / 2;
+            this.ySpeed = 0;
+            this.jumping = false;
+        }
+        else {
+            this.ySpeed += 1;
+        }
+    }
+}
 var KeyCode;
 (function (KeyCode) {
-    KeyCode[KeyCode["W"] = 119] = "W";
-    KeyCode[KeyCode["A"] = 97] = "A";
-    KeyCode[KeyCode["S"] = 115] = "S";
-    KeyCode[KeyCode["D"] = 100] = "D";
+    KeyCode[KeyCode["W"] = 87] = "W";
+    KeyCode[KeyCode["A"] = 65] = "A";
+    KeyCode[KeyCode["S"] = 83] = "S";
+    KeyCode[KeyCode["D"] = 68] = "D";
 })(KeyCode || (KeyCode = {}));
-var KeyState = (function () {
-    function KeyState() {
+class KeyState {
+    constructor() {
+        this.isDown = (key) => {
+            return this.state[key];
+        };
+        this.onKeydown = (event) => {
+            this.state[event.keyCode] = true;
+        };
+        this.onKeyup = (event) => {
+            this.state[event.keyCode] = false;
+        };
         this.state = {};
     }
-    KeyState.prototype.isDown = function (key) {
-        return this.state[key];
-    };
-    KeyState.prototype.onKeydown = function (event) {
-        this.state[event.keyCode] = true;
-    };
-    KeyState.prototype.onKeyup = function (event) {
-        this.state[event.keyCode] = false;
-    };
-    KeyState.W = 119;
-    KeyState.A = 97;
-    KeyState.S = 115;
-    KeyState.D = 100;
-    return KeyState;
-}());
+}
 // Global setup
-var player = new Player();
-player.x = WIDTH / 2;
-player.y = HEIGHT / 2;
-var keyState = new KeyState();
+const player = new Player();
+const level = new Level();
+const keyState = new KeyState();
 window.addEventListener("keydown", keyState.onKeydown, false);
-window.addEventListener("keydup", keyState.onKeyup, false);
+window.addEventListener("keyup", keyState.onKeyup, false);
 // Game functions
 function mainLoop(timestamp) {
     clearScreen();
     update(timestamp);
+    level.draw(context);
     player.draw();
     requestAnimationFrame(mainLoop);
 }
 function update(timestamp) {
-    if (keyState.isDown(KeyCode.A)) {
-        --player.x;
-    }
-    else if (keyState.isDown(KeyCode.D)) {
-        ++player.x;
-    }
-    else if (keyState.isDown(KeyCode.S)) {
-        ++player.y;
-    }
-    else if (keyState.isDown(KeyCode.W)) {
-        --player.y;
-    }
-}
-function handleInput(event) {
-    var keyCode = event.keyCode;
-    alert(keyCode);
+    player.update(keyState);
 }
 function clearScreen() {
     context.save();
--- game.ts
+++ game.ts
@@ -4,12 +4,68 @@ const context = canvas.getContext("2d") as CanvasRenderingContext2D;
 // Game constants
 const WIDTH = 720;
 const HEIGHT = 480;
-const TILE_SIZE = 32;
+const TILE_SIZE = 24;
+
+class Level {
+  readonly data = [
+    "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    "X                            X",
+    "X                            X",
+    "X                            X",
+    "X                            X",
+    "X                            X",
+    "X                            X",
+    "X                            X",
+    "X                            X",
+    "X                            X",
+    "X                            X",
+    "X                            X",
+    "X                            X",
+    "X                            X",
+    "X                            X",
+    "X                            X",
+    "X                           XX",
+    "X           XXXX  XXXX     XXX",
+    "X           X  X  X  X    XXXX",
+    "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+  ];
+
+  public draw(context: CanvasRenderingContext2D) {
+    context.save();
+    context.fillStyle = "#0000FF";
+    context.strokeStyle = "#FFFFFF";
+
+    let y = 0 * TILE_SIZE;
+
+    this.data.forEach(rowData => {
+      for (let col = 0; col < 30; ++col) {
+        if (rowData[col] === "X") {
+          context.fillRect(col * TILE_SIZE, y, TILE_SIZE, TILE_SIZE);
+        }
+      }
+
+      y += TILE_SIZE;
+    });
+
+    context.restore();
+  }
+}
 
 // Supporting classes
 class Player {
   x: number;
   y: number;
+  ySpeed: number;
+  xSpeed: number;
+  jumping: boolean;
+
+  constructor() {
+    this.x = WIDTH / 2;
+    this.y = HEIGHT - TILE_SIZE / 2;
+    this.ySpeed = 0;
+    this.xSpeed = 3.5;
+    this.jumping = false;
+  }
 
   public draw() {
     context.save();
@@ -22,46 +78,65 @@ class Player {
     );
     context.restore();
   }
+
+  public update(keyState: KeyState) {
+    if (keyState.isDown(KeyCode.A)) {
+      this.x -= this.xSpeed;
+    } else if (keyState.isDown(KeyCode.D)) {
+      this.x += this.xSpeed;
+    } else if (keyState.isDown(KeyCode.W)) {
+      if (this.ySpeed === 0 && !this.jumping) {
+        this.ySpeed = -15;
+        this.jumping = true;
+      }
+    }
+
+    this.y += this.ySpeed;
+
+    if (this.y > HEIGHT - TILE_SIZE / 2) {
+      this.y = HEIGHT - TILE_SIZE / 2;
+      this.ySpeed = 0;
+      this.jumping = false;
+    } else {
+      this.ySpeed += 1;
+    }
+  }
 }
 
 enum KeyCode {
-  W = 119,
-  A = 97,
-  S = 115,
-  D = 100
+  W = 87,
+  A = 65,
+  S = 83,
+  D = 68
 }
 
 class KeyState {
-  static readonly W = 119;
-  static readonly A = 97;
-  static readonly S = 115;
-  static readonly D = 100;
+  state: { [keyCode: number]: boolean };
 
-  private state: { [keyCode: number]: boolean } = {};
+  constructor() {
+    this.state = {};
+  }
 
-  public isDown(key: KeyCode): boolean {
+  public isDown = (key: KeyCode): boolean => {
     return this.state[key];
-  }
+  };
 
-  public onKeydown(event: KeyboardEvent) {
+  public onKeydown = (event: KeyboardEvent): void => {
     this.state[event.keyCode] = true;
-  }
+  };
 
-  public onKeyup(event: KeyboardEvent) {
+  public onKeyup = (event: KeyboardEvent): void => {
     this.state[event.keyCode] = false;
-  }
+  };
 }
 
 // Global setup
 const player = new Player();
-
-player.x = WIDTH / 2;
-player.y = HEIGHT / 2;
-
+const level = new Level();
 const keyState = new KeyState();
 
 window.addEventListener("keydown", keyState.onKeydown, false);
-window.addEventListener("keydup", keyState.onKeyup, false);
+window.addEventListener("keyup", keyState.onKeyup, false);
 
 // Game functions
 
@@ -70,27 +145,14 @@ function mainLoop(timestamp: number): void {
 
   update(timestamp);
 
+  level.draw(context);
   player.draw();
 
   requestAnimationFrame(mainLoop);
 }
 
 function update(timestamp: number): void {
-  if (keyState.isDown(KeyCode.A)) {
-    --player.x;
-  } else if (keyState.isDown(KeyCode.D)) {
-    ++player.x;
-  } else if (keyState.isDown(KeyCode.S)) {
-    ++player.y;
-  } else if (keyState.isDown(KeyCode.W)) {
-    --player.y;
-  }
-}
-
-function handleInput(event: any) {
-  const keyCode: number = event.keyCode;
-
-  alert(keyCode);
+  player.update(keyState);
 }
 
 function clearScreen() {
